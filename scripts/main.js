7a56fb54cf20199019bbf2eb4f90e84b75f97395
Jonas Nunes <jonas.nunees@yahoo.com.br>
2023-04-11 21:50:37 -0300

    Criação do arquivo main.js contendo as funcionalidades do menu responsivo e de alterar a cor do header quando scrollar a página

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+// menu responsivo
+const btnMobile = document.getElementById('btn-mobile')
+
+function toggleMenu(event){
+    if (event.type === 'touchstart') event.preventDefault ();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expended', 'active');
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+// mudar a cor do header após rolagem da página
+function onScroll(){
+
+    if(scrollY > 0){
+        menuResponsivo.classList.add('scroll')   
+    } else{
+        menuResponsivo.classList.remove('scroll')
+    }
+}
